863479ac9732dec3cad4173895b3d3cc4f8eeb29
davidtaing <adavidtaing@gmail.com>
2023-04-15 21:16:28 +1000

    feat: add postgres connection

---
+++ database.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"sync"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+func GetDB() *sql.DB {
+	connStr := "host=localhost port=5432 user=myuser password=mypass dbname=restapi sslmode=disable"
+	DB, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return DB
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/davidtaing/chi-server-tutorial
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.8 
+require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/lib/pq v1.10.8
