77afdd4d035f86a2dfedfb033d3aabb79d1f9d64
davidtaing <adavidtaing@gmail.com>
2023-04-17 20:56:52 +1000

    feat: add test post endpoint

--- server.go
+++ server.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+type RequestBody struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
+	})
+
+	r.Post("/posts", func(w http.ResponseWriter, r *http.Request) {
+		var reqBody RequestBody
+		err := json.NewDecoder(r.Body).Decode(&reqBody)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Bad Request"))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		err = json.NewEncoder(w).Encode(reqBody)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	})
 
 	http.ListenAndServe(":3000", r)
